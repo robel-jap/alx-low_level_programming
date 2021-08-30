ee301f50d8105412a254080421eed614eba3d361
robel <robela149@gmail.com>
2021-08-30 06:15:57 -0700

    print from one to 9 two

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,22 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
  * main - Entry point
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-  int n;
+	int i;
 
-  for (n = 0; n < 10; n++)
-    printf("%d", n);
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
 
-  printf("\n");
-
-  return(0);
-
+	return (0);
 }
