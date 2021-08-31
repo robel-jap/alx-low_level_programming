302270588705d262c9c68864f3f74c779dea87d2
robel <robela149@gmail.com>
2021-08-31 05:58:43 -0700

    print alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+
+void print_alphabet(void)
+{
+	char i;
+	int x;
+
+	for (x = 0; x < 10; x++) 
+	  {
+	    for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	    _putchar('\n');
+	  }
+}
